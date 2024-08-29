a65bed862ce3a353c5f51feeacf38e299d394930
Duy <you@example.com>
2024-08-29 21:48:20 +0700

    update2

--- GAME/tesssst/test.py
+++ GAME/tesssst/test.py
@@ -1,80 +1,66 @@
 import pygame, sys
-from pygame.examples.grid import WINDOW_WIDTH
 from pygame.locals import *
 import random
 
+# Constants
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 400
-
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
-GREEN = (0, 255, 0)
+GROUND_HEIGHT = 100
 
 pygame.init()
-setGround = pygame.Rect((0,300, WINDOW_WIDTH, 100))
 
+# Ground setup
+setGround = pygame.Rect((0, WINDOW_HEIGHT - GROUND_HEIGHT, WINDOW_WIDTH, GROUND_HEIGHT))
+
+# Block class
 class Block():
-    def __init__(self, x,y,width,height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-    def  current_pos(self):
-        return self.rect.y
+    def __init__(self, x, y, width, height, color):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.fall_speed = random.randint(1, 3)  # Random fall speed
+
     def fall(self):
-        block = pygame.draw.rect(screen, RED, self.rect)
-        if block.bottom <= 260:
-            self.rect.y +=1
-        if block.bottom >260:
-            self.rect.y = 0
+        if self.rect.bottom < WINDOW_HEIGHT - GROUND_HEIGHT:
+            self.rect.y += self.fall_speed
 
-
-
-
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
 
 ### FPS ###
 FPS = 60
 fpsClock = pygame.time.Clock()
 
+# Screen setup
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption('Test game')
+pygame.display.set_caption('Test Game')
 
-x,y = (0,0)
-block = pygame.Rect(0,0,40,40)
+# Create blocks with independent properties
+Block_List = [
+    Block(0, 0, 40, 40, RED),
+    Block(50, 0, 40, 40, RED),
+    Block(100, 0, 40, 40, RED),
+    Block(150, 20, 40, 40, RED)
+]
 
-rand = 0
-
-Block_List = []
-
-
-Block1 = Block(0,0,40,40)
-Block2 = Block(40,0,40,40)
-Block3 = Block(80,0,40,40)
-Block4 = Block(120,20,40,40)
-
-Block_List.append(Block1)
-Block_List.append(Block2)
-Block_List.append(Block3)
-Block_List.append(Block4)
-
+# Main game loop
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    # Fill screen with white
     screen.fill(WHITE)
-    # block2 = pygame.Surface((40,40))
-    # block2.fill(RED)
-    #
-    # screen.blit(block2,(0,0))
-    # block1 = pygame.draw.rect(screen,RED,block)
-    GROUND = pygame.draw.rect(screen,'#571e15',setGround)
-    current_time = pygame.time.get_ticks()
-    if Block_List[rand].current_pos() <= 150:
-        Block_List[rand].fall()
-    else:
-        rand = random.randint(0, 3)
+
+    # Draw the ground
+    GROUND = pygame.draw.rect(screen, '#571e15', setGround)
+
+    # Update and draw each block independently
+    for block in Block_List:
+        block.fall()
+        block.draw(screen)
 
     pygame.display.update()
     fpsClock.tick(FPS)
